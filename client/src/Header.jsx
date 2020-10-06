6f1e5c9941af8fd6339682810150bc519d289e33
Eric Dimech <ericdimech@hotmail.com>
2020-10-05 22:02:08 -0400

    Added Header file

--- client/src/Header.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-import Logo from './assets/Logo/InStock-Logo_2x.png';
-
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import Logo from './assets/Logo/InStock-Logo_2x.png';
+
+class Navbar extends Component {
+    render () {
+        return (
+            <div className='navbar'>
+                <div>
+                    <NavLink to='/warehouses' className='navbar__logo'>
+                        <img src={ Logo } className='navbar__img' alt='Instock' />
+                    </NavLink>
+                </div>
+                <div className='navbar__links'>
+                    <ul className='navbar__list'>
+                        <NavLink to='/inventory' className='navbar__inventory'></NavLink>
+                        <NavLink to='/warehouses' className='navbar__warehouses'></NavLink>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+};
+
+export default Navbar;
