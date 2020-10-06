48d92add12b71a4ee8b88e2bdbd3bfa165cd99ba
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2020-10-06 00:23:20 -0400

    Warehouses List Header and List Item Complete With Styling

--- client/src/components/WarehouseListItem/WarehouseListItem.jsx
+++ client/src/components/WarehouseListItem/WarehouseListItem.jsx
@@ -8,34 +8,38 @@ import './warehouse-list-item.scss';
 function WarehouseListItem() {
     return (
         <>
-        <div className="warehouse-list">
-            <div className="warehouse-list-info">
-                <p className="warehouse-list-info-heading">WAREHOUSE</p>
-                <Link to='/'>
-                    <p className="warehouse-list__city">Manhattan</p>
-                    <img src={Arrow} alt="Arrow Icon" className="warehouse-list__icon warehouse-list__icon-arrow"/>
-                </Link>
+            <div className="warehouse-list">
+                <div className="warehouse-list__container">
+                    <div className="warehouse-list-info">
+                        <h4 className="warehouse-list-info-heading">WAREHOUSE</h4>
+                        <div className="warehouse-list__city-container">
+                            <Link to='/'>
+                                <p className="warehouse-list__city">Manhattan</p>
+                            </Link>
+                                <img src={Arrow} alt="Arrow Icon" className="warehouse-list__icon warehouse-list__icon-arrow"/>
+                        </div>
+                    </div>
+                    <div className="warehouse-list-info">
+                        <h4 className="warehouse-list-info-heading">ADDRESS</h4>
+                        <p className="warehouse-list__address">503 Broadway, New York, USA</p>
+                    </div>
+                    <div className="warehouse-list-info">
+                        <h4 className="warehouse-list-info-heading">CONTACT NAME</h4>
+                        <p className="warehouse-list__contact-name">Parmin Aujila</p>
+                    </div>
+                    <div className="warehouse-list__contact-info">
+                        <div className="warehouse-list-info">
+                            <h4 className="warehouse-list-info-heading">CONTACT INFORMATION</h4>
+                            <p className="warehouse-list__contact-info-phone">+ 1 (629) 555-0129</p>
+                            <p className="warehouse-list__contact-info-email">paujila@instock.com</p>
+                        </div>      
+                    </div>
+                </div>
+                <div className="warehouse-list__icon-container">
+                    <img src={Delete} alt="Delete Icon" className="warehouse-list__icon warehouse-list__icon-delete"/>
+                    <img src={Edit} alt="Edit Icon" className="warehouse-list__icon warehouse-list__icon-edit"/>
+                </div>
             </div>
-            <div className="warehouse-list-info">
-                <p className="warehouse-list-info-heading">ADDRESS</p>
-                <p className="warehouse-list__address">503 Broadway, New York, USA</p>
-            </div>
-            <div className="warehouse-list-info">
-                <p className="warehouse-list-info-heading">CONTACT NAME</p>
-                <p className="warehouse-list__contact-name">Parmin Aujila</p>
-            </div>
-            <div className="warehouse-list__contact-info">
-                <div className="warehouse-list-info">
-                    <p className="warehouse-list-info-heading">CONTACT INFORMATION</p>
-                    <p className="warehouse-list__contact-info-phone">+ 1 (629) 555-0129</p>
-                    <p className="warehouse-list__contact-info-email">paujila@instock.com</p>
-                </div>      
-            </div>
-        </div>
-        <div className="warehouse-list__icon-container">
-            <img src={Delete} alt="Delete Icon" className="warehouse-list__icon warehouse-list__icon-delete"/>
-            <img src={Edit} alt="Edit Icon" className="warehouse-list__icon warehouse-list__icon-edit"/>
-        </div>
         </>
     )
 };
