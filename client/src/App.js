7e2cd74df78fa28cf07b53130dc7f5d47b0b8d43
Jaycee Bam <jaycee.bam@outlook.com>
2020-10-07 09:17:12 -0700

    Delete item mobile view

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import DeleteItem from './DeleteItem/DeleteItem';
 
 function App() {
   return (
     <div className="App">
+      <DeleteItem />
     </div>
   );
 }
