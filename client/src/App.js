60b2b037a8601ca9c9e84a6f12763da823187bab
Eric Dimech <ericdimech@hotmail.com>
2020-10-06 22:10:51 -0400

    Renamed folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import EditWarehouse from './components/editWarehouse/EditWarehouse';
+import EditWarehouse from './components/EditWarehouse/EditWarehouse';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
