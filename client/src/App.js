3da4315b3e76afe45e90a4d260ab4f0419f298a5
Eric Dimech <ericdimech@hotmail.com>
2020-10-06 22:06:31 -0400

    Built EditWarehouse function

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import EditWarehouse from './components/editWarehouse/EditWarehouse';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
@@ -8,6 +9,7 @@ class App extends React.Component {
   return (
     <Router>
       <Header />
+      <EditWarehouse />
       <Switch>
         <div>Hello World</div>
       </Switch>
