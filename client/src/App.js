de2d9ae0b0e53319d36ce382842f581aadd1d3ea
abdul farhan <kukluu@hotmail.com>
2020-10-08 09:59:07 -0700

    kept Route in import App.js , no changes made

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import './App.scss';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import "./App.scss";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends React.Component {
   render() {
-  return (
-    <Router>
-      <Header />
-      <Footer />
-      <Switch>
-        <div>Happy Wednesday</div>
-      </Switch>
-    </Router>
-  );
- };
-};
+    return (
+      <Router>
+        <Header />
+        <Footer />
+        <Switch>
+          <div>Happy Wednesday</div>
+        </Switch>
+      </Router>
+    );
+  }
+}
 
 export default App;
