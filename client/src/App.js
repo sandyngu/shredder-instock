ede8e3b1dc2d1cd69fa05cdb29cdaa2eb22a132f
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2020-10-08 17:19:27 -0400

    Merged with Development

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,17 @@
-<<<<<<< HEAD
 import React from 'react';
-import './App.scss';
-import DeleteWarehouse from './components/DeleteWarehouse/DeleteWarehouse';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-=======
-import React from "react";
-import "./App.scss";
-import Header from "./components/Header/Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import InventoryList from "./components/InventoryList/InventoryList";
 // import WarehousesList from "./components/WarehousesList/WarehousesList";
->>>>>>> development
+import DeleteWarehouse from './components/DeleteWarehouse/DeleteWarehouse';
+import Header from "./components/Header/Header";
+import './App.scss';
+
 
 class App extends React.Component {
   render() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route path='/warehouses/delete-warehouse' component={DeleteWarehouse}/>
         <Route
