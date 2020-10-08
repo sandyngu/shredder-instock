c195242001e99fb90d372c1ae7561a0732769fa3
Jaycee Bam <jaycee.bam@outlook.com>
2020-10-08 13:38:43 -0700

    More inventory form styling

--- client/src/components/ItemInputForm/ItemInputForm.jsx
+++ client/src/components/ItemInputForm/ItemInputForm.jsx
@@ -8,52 +8,62 @@ function ItemInputForm() {
     return (
         <>
             <form className="item-form">
-                <div className="item-form__details">
-                <h2 className="item-form__title">Item Details</h2>
-                <label className="item-form__label">Item Name</label>
-                <input className="item-form__input" type="text" placeHolder="Item Name" name="name" />
-                <label className="item-form__label">Description</label>
-                <textarea className="item-form__description-input" type="text" placeHolder="Please enter a brief item description..." name="description" />
-                <label className="item-form__label">Category</label>
-                <select className="item-form__dropdown" placeHolder="Please Select">
-                    <option value="">Please select</option>
-                    <option value="Electronics">Electronics</option>
-                    <option value="Gear">Gear</option>
-                    <option value="Apparel">Apparel</option>
-                    <option value="Accessories">Accessories</option>
-                    <option value="Health">Health</option>
-                </select>
+                <div className="item-form__masterdiv">
+                    <div className="item-form__details">
+                        <h2 className="item-form__title">Item Details</h2>
+                        <label className="item-form__label">Item Name</label>
+                        <input className="item-form__input" type="text" placeHolder="Item Name" name="name" />
+                        <label className="item-form__label">Description</label>
+                        <textarea className="item-form__description-input" type="text" placeHolder="Please enter a brief item description..." name="description" />
+                        <label className="item-form__label">Category</label>
+                        <select className="item-form__dropdown" placeHolder="Please Select">
+                            <option value="">Please select</option>
+                            <option value="Electronics">Electronics</option>
+                            <option value="Gear">Gear</option>
+                            <option value="Apparel">Apparel</option>
+                            <option value="Accessories">Accessories</option>
+                            <option value="Health">Health</option>
+                        </select>
+                    </div>
+                    <div className="item-form__line"></div>
+                    <div className="item-form__availability">
+                        <h2 className="item-form__title">Item Availability</h2>
+                        <label className="item-form__label">Status</label>
+                        <div className="item-form__status">
+                            <div className="item-form__status-subcontainer">
+                                <input className="item-form__status-subcontainer-option" type="radio" value="In Stock" name="stockStatus" />
+                                <p className="item-form__status-subcontainer-text">In stock</p>
+                            </div>
+                            <div className="item-form__status-subcontainer">
+                                <input className="item-form__status-subcontainer-option" type="radio" value="Out of Stock" name="stockStatus" />
+                                <p className="item-form__status-subcontainer-text">Out of stock</p>
+                            </div>
+                        </div>
+                        <label className="item-form__label">Quantity</label>
+                        <input className="item-form__input" type="text" placeHolder="0" name="quantity" />
+                        <label className="item-form__label">Warehouse</label>
+                        <select className="item-form__dropdown">
+                            <img src={Dropdown} />
+                            <option value="">Please select</option>
+                            <option value="Manhatten">Manhatten</option>
+                            <option value="King West">King West</option>
+                            <option value="Granville">Granville</option>
+                            <option value="San Fran">San Fran</option>
+                            <option value="Santa Monica">Santa Monica</option>
+                            <option value="Seattle">Seattle</option>
+                            <option value="Montrea">Montreal</option>
+                            <option value="Boston">Boston</option>
+                        </select>
+                    </div>
                 </div>
-            <div className="item-form__availability">
-                <h2 className="item-form__title">Item Availability</h2>
-                <label className="item-form__label">Status</label>
-                <div className="item-form__status-container">
-                    <input className="item-form__status-option" type="radio" value="In Stock" name="stockStatus"/>
-                    <p>In stock</p> 
-                    <input className="item-form__status-option" type="radio" value="Out of Stock" name="stockStatus"/>
-                    <p>Out of stock</p>
-                </div>
-                <label className="item-form__label">Quantity</label>
-                <input className="item-form__input" type="text" placeHolder="0" name="quantity" />
-                <label className="item-form__label">Warehouse</label>
-                <select className="item-form__dropdown">
-                    <img src={Dropdown}/>
-                    <option value="">Please select</option>
-                    <option value="Manhatten">Manhatten</option>
-                    <option value="King West">King West</option>
-                    <option value="Granville">Granville</option>
-                    <option value="San Fran">San Fran</option>
-                    <option value="Santa Monica">Santa Monica</option>
-                    <option value="Seattle">Seattle</option>
-                    <option value="Montrea">Montreal</option>
-                    <option value="Boston">Boston</option>
-                </select>
                 <div className="item-form__button">
-                    <button className="item-form__button-cancel">Cancel</button>
-                    <button className="item-form__button-add">+ Add Item</button>
+                    <div className="item-form__button-box">
+                        <button className="item-form__button-cancel">Cancel</button>
+                        <button className="item-form__button-add">+ Add Item</button>
+                    </div>
                 </div>
-            </div>
-        </form>
+
+            </form>
         </>
     )
 
