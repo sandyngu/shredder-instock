93cf0b9c512da56b2cc48fe8f1ef97f3784ce877
Eric Dimech <ericdimech@hotmail.com>
2020-10-08 21:44:28 -0400

    fixing errors

--- client/src/components/WarehouseListItem/WarehouseListItem.jsx
+++ client/src/components/WarehouseListItem/WarehouseListItem.jsx
@@ -10,10 +10,7 @@ function WarehouseListItem(props) {
     return (
         <>
             {props.warehousesList.map(warehouse =>
-<<<<<<< HEAD
             <>
-=======
->>>>>>> development
             <div className="warehouse-list" key={warehouse.id}>
                 <div className="warehouse-list__container">
                     <div className="warehouse-list-info">
@@ -58,10 +55,7 @@ function WarehouseListItem(props) {
                     </Link>
                 </div>
             </div>
-<<<<<<< HEAD
             </>
-=======
->>>>>>> development
             )}
         </>
     )
