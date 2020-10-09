817e975cb46c108818afc7cbf03c3af2afa1d910
Jaycee Bam <jaycee.bam@outlook.com>
2020-10-09 11:09:55 -0700

    Backend functionality improved

--- server/routes/inventories_routes.js
+++ server/routes/inventories_routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { route } = require('./inventories_routes');
 const router = express.Router();
-const inventories = require("../inventories.json");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const fs = require('fs');
@@ -13,6 +12,7 @@ router.use(express.json());
 // Get inventory objects
 
 router.get('/', (req, res) => {
+    const inventories = JSON.parse(fs.readFileSync("../inventories.json"));
     res.send(inventories);
 })
 
@@ -25,18 +25,11 @@ router.get('/:id', (req, res) => {
 // Create inventory objects and write to JSON file
 
 router.post('/', (req, res) => {
-    console.log(req.body);
-    const addItem = req.body;
-    const stringAddItem = JSON.stringify(addItem);
-    console.log(stringAddItem);
-    const inventoriesData = JSON.parse(fs.readFileSync('./inventories.json'));
-    inventoriesData.push(addItem);
-    fs.writeFileSync('./inventories.json', JSON.stringify(inventoriesData), null, 2);
-        
-
-
+    const inventoriesData = JSON.parse(fs.readFileSync('../inventories.json'));
+    inventoriesData.push(req.body);
+    fs.writeFileSync('../inventories.json', JSON.stringify(inventoriesData), null, 2);
+    res.status(201).send({status:'object created'});
 })
-
 
 // Edit inventory objects
 
