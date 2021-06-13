e110d8bec98ae286a4e728d4b9850f0ff7731e83
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2021-06-13 12:09:09 -0400

    Updates

---
+++ precache-manifest.b215e85bfb6b9106124c581328aeaf44.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648879a628468bfca7203bf75b4ba81e",
+    "url": "/shredder-instock/index.html"
+  },
+  {
+    "revision": "7b2c01385a174f394d16",
+    "url": "/shredder-instock/static/css/main.a11ae52a.chunk.css"
+  },
+  {
+    "revision": "61e9917c6dffcb62c866",
+    "url": "/shredder-instock/static/js/2.96691b60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shredder-instock/static/js/2.96691b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2c01385a174f394d16",
+    "url": "/shredder-instock/static/js/main.c2661200.chunk.js"
+  },
+  {
+    "revision": "8a054dc97951b48f74a6",
+    "url": "/shredder-instock/static/js/runtime-main.9168b42e.js"
+  },
+  {
+    "revision": "ed1d014bf2b8b72f27d6af65c69f710a",
+    "url": "/shredder-instock/static/media/TitilliumWeb-Regular.ed1d014b.ttf"
+  },
+  {
+    "revision": "ce96d75e97d58b7396e1431557bb02a0",
+    "url": "/shredder-instock/static/media/TitilliumWeb-SemiBold.ce96d75e.ttf"
+  },
+  {
+    "revision": "eb4230547888f1a68d4fc0f6cc4e7539",
+    "url": "/shredder-instock/static/media/arrow_back-24px.eb423054.svg"
+  },
+  {
+    "revision": "f0484b50a4850b3c9e91052d20fa64c0",
+    "url": "/shredder-instock/static/media/arrow_drop_down-24px.f0484b50.svg"
+  },
+  {
+    "revision": "c2293c0bbe45fd1dfb813476f3483aee",
+    "url": "/shredder-instock/static/media/chevron_right-24px.c2293c0b.svg"
+  },
+  {
+    "revision": "c44f8750cb24799348617c8ab18d4adb",
+    "url": "/shredder-instock/static/media/close-24px.c44f8750.svg"
+  },
+  {
+    "revision": "f26a36e261de28bc1a77d76cbbb29140",
+    "url": "/shredder-instock/static/media/delete_outline-24px.f26a36e2.svg"
+  },
+  {
+    "revision": "4a6f685286bb93a69c5958aa58378651",
+    "url": "/shredder-instock/static/media/edit-24px.4a6f6852.svg"
+  },
+  {
+    "revision": "bac370a52abf1f3fc15916c8441b8107",
+    "url": "/shredder-instock/static/media/error-24px.bac370a5.svg"
+  },
+  {
+    "revision": "b40f4155297b75ce70a72669946ab0d8",
+    "url": "/shredder-instock/static/media/search-24px.b40f4155.svg"
+  },
+  {
+    "revision": "0815bdb68cb0f49097b9dea700a2257b",
+    "url": "/shredder-instock/static/media/sort-24px.0815bdb6.svg"
+  }
+]);
